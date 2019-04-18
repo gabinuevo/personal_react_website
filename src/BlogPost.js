499ef0337d0fee695426b85bd650cae47a085126
Gabriela <balicas.gb@gmail.com>
2019-04-17 20:09:13 -0700

    start blog modules

---
+++ src/BlogPost.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './BlogPost.css'
+
+class BlogPost extends Component {
+    render() {
+        return (
+            <div className="BlogPost">
+                <div id="paper">
+                    <p className="BlogPost-title">TITLE</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BlogPost;
