0d3e3f0343c6477abfda62e129be030df7db91de
Gabriela <balicas.gb@gmail.com>
2019-05-07 12:29:33 -0700

    update svg breakpoints and update projects

--- src/ProjectInfo.js
+++ src/ProjectInfo.js
@@ -1,23 +1,23 @@
 export const projectInfo = {
   proj1: {
+    title: "The Relish",
+    description: "A content management system for The Relish SF.",
+    technologies: "Built with React, Material UI, and Firebase.",
+    link: "https://www.youtube.com/watch?v=R_sNOr_X3LY&feature=youtu.be",
+    github: null
+  },
+  proj2: {
     title: "MicroBlog",
-    description: "Inspired by wordpress",
+    description: "Inspired by wordpress.",
     technologies: "Built with React and Redux.",
     link: null,
     github: "https://github.com/gabinuevo/microblog/tree/master"
   },
-  proj2: {
+  proj3: {
     title: "Flower Puzzle",
-    description: `A spin on “Light’s Out”`,
-    technologies: "Built with React",
+    description: `A spin on “Light’s Out.”`,
+    technologies: "Built with React.",
     link: "https://gabinuevo.github.io/flower-puzzle/",
     github: "https://gabinuevo.github.io/flower-puzzle/"
-  },
-  proj3: {
-    title: "Warbler",
-    description: "A twitter clone",
-    technologies: "Built with Python, Flask, and PostgreSQL",
-    link: "https://warble-warble.herokuapp.com/",
-    github: "https://github.com/gabinuevo/warble-warble/tree/master"
   }
 }
