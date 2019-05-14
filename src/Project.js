a9b81964e63551b8e2fb8cbc016a2cbbbd0382bd
Gabriela <balicas.gb@gmail.com>
2019-05-14 12:23:43 -0700

    fix bug w/ Project component

--- src/Project.js
+++ src/Project.js
@@ -19,7 +19,7 @@ class Project extends Component {
               <p>{description}</p>
               <p>{technologies}</p>
               <a href={link}>{link && "Demo"}</a>
-              <a href={github}>Github</a>
+              <a href={github}>{ github && "Github"}</a>
             </div>
 
             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 659 522" enableBackground="new 0 0 659 522" xmlSpace="preserve" preserveAspectRatio="none" className="svg">
